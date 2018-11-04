6729a035dfc3ef5e1027b90a7e82992130c78845
kojoma <masaru.kojo@gmail.com>
2018-11-04 10:51:36 +0900

    add camera picker

--- TestBitrise/ViewController.swift
+++ TestBitrise/ViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UIGestureRecognizerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
-    var picker: UIImagePickerController! = UIImagePickerController()
+    @IBOutlet weak var cameraView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,27 +24,57 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
 
     private func setupGesture() {
-        let gesture: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.onTapImageView(_:)))
-        gesture.delegate = self
-        self.imageView.addGestureRecognizer(gesture)
+        let imageGesture: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.onTapImageView(_:)))
+        imageGesture.delegate = self
+        self.imageView.addGestureRecognizer(imageGesture)
         self.imageView.isUserInteractionEnabled = true
+        
+        let cameraGesture: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.onTapCameraView(_:)))
+        cameraGesture.delegate = self
+        self.cameraView.addGestureRecognizer(cameraGesture)
+        self.cameraView.isUserInteractionEnabled = true
     }
 
     @objc private func onTapImageView(_ sender: UITapGestureRecognizer) {
         if sender.state == .ended {
-            self.picker.sourceType = UIImagePickerControllerSourceType.photoLibrary
-            self.picker.delegate = self
-            
-            self.picker.navigationBar.tintColor = UIColor.white
-            self.picker.navigationBar.barTintColor = UIColor.gray
-            
-            present(self.picker, animated: true, completion: nil)
+            let sourceType: UIImagePickerController.SourceType = UIImagePickerController.SourceType.photoLibrary
+            if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.photoLibrary) {
+                let photoPicker = UIImagePickerController()
+                photoPicker.sourceType = sourceType
+                photoPicker.delegate = self
+                photoPicker.navigationBar.tintColor = UIColor.white
+                photoPicker.navigationBar.barTintColor = UIColor.gray
+                
+                self.present(photoPicker, animated: true, completion: nil)
+            }
         }
     }
-    
+
+    @objc private func onTapCameraView(_ sender: UITapGestureRecognizer) {
+        if sender.state == .ended {
+            let sourceType: UIImagePickerController.SourceType = UIImagePickerController.SourceType.camera
+            if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.camera) {
+                let cameraPicker = UIImagePickerController()
+                cameraPicker.sourceType = sourceType
+                cameraPicker.delegate = self
+                cameraPicker.navigationBar.tintColor = UIColor.white
+                cameraPicker.navigationBar.barTintColor = UIColor.gray
+
+                self.present(cameraPicker, animated: true, completion: nil)
+            }
+        }
+    }
+
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
-            self.imageView.image = image
+            switch picker.sourceType {
+            case UIImagePickerController.SourceType.photoLibrary:
+                self.imageView.image = image
+            case UIImagePickerController.SourceType.camera:
+                self.cameraView.image = image
+            default:
+                break
+            }
         } else {
             print("Error")
         }
